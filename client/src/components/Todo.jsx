11f0f98e452a328d8bc16cb11ed8fe254fd67f73
Ojas1510 <ojasnagta@gmail.com>
2024-01-30 14:35:52 +0530

    initial commit

---
+++ client/src/components/Todo.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+
+import { toggleTodo, updateTodo } from "../redux/actions";
+import { deleteTodo } from "../redux/actions";
+
+import { useDispatch } from "react-redux";
+
+const Todo = ({ todo }) => {
+
+    const [editing, setEditing] = useState(false);
+    const [text, setText] = useState(todo?.data);
+
+    const dispatch = useDispatch();
+
+    const onFormSubmit = (e) => {
+        e.preventDefault();
+
+        setEditing(prevState => !prevState);
+
+        dispatch(updateTodo(todo._id, text))
+    }
+
+    return (
+        <li
+        className="task"
+        onClick={() => dispatch(toggleTodo(todo._id))}
+        style={{
+            backgroundColor: todo?.done ? '#46d052' : '#ADD8E6', // Change color property
+            color: todo?.done ? 'black' : 'inherit', // Adjust text color if needed
+        }}
+        data-testid="todo-test"
+    >
+            <span style={{ display: editing ? 'none' : '' }}>{todo?.data}</span>
+
+            <form
+                style={{ display: editing ? 'inline' : 'none' }}
+                onSubmit={onFormSubmit}
+            >
+                <input
+                    type="text"
+                    value={text}
+                    className="edit-todo"
+                    onChange={(e) => setText(e.target.value)}
+                />
+            </form>
+
+            <span className="icon" onClick={() => dispatch(deleteTodo(todo._id))}>
+                <i className="fas fa-trash" />
+            </span>
+            <span className="icon" onClick={() => setEditing(prevState => !prevState)}>
+                <i className="fas fa-pen" />
+            </span>
+        </li>
+    )
+}
+
+export default Todo;
